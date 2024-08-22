96893c1070257a4ae137bab85b2f04b49faa1e2b
james-eo <jameseokonkwo@gmail.com>
2024-08-22 18:07:46 +0100

    Updated task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,20 +1,11 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
-  it('should return 4 when inputs are 1 and 3', () => {
+describe('calculateNumber', function() {
+  it('should return the sum of rounded numbers', function() {
     assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-
-  it('should return 5 when inputs are 1 and 3.7', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-
-  it('should return 5 when inputs are 1.2 and 3.7', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-
-  it('should return 6 when inputs are 1.5 and 3.7', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
